7b056bbce806eecfcf7e30eaeab52cdb6433119f
Lee <ab4004@naver.com>
2024-01-25 17:46:54 +0900

    전적검색 업데이트
    검색했던 소환사의 매치기록 20개와 각 기록별 게임모드, 사용 챔피언, 사용 스펠, 승리 여부, KDA가 출력됩니다.

--- src/main/java/com/ezpick/lol/dto/ObjectivesDTO.java
+++ src/main/java/com/ezpick/lol/dto/ObjectivesDTO.java
@@ -1,5 +1,7 @@
 package com.ezpick.lol.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ObjectivesDTO {
 	private ObjectiveDTO baron;
 	private ObjectiveDTO champion;
--- src/main/java/com/ezpick/lol/dto/PerkStyleDTO.java
+++ src/main/java/com/ezpick/lol/dto/PerkStyleDTO.java
@@ -1,5 +1,7 @@
 package com.ezpick.lol.dto;
 
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +11,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class PerkStyleDTO {
 	private String description;
-	private int selections;
+	private List<PerkStyleSelectionDTO> selections;
 	private int style;
 }
