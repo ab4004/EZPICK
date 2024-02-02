b0330760aa3aae592d1ac661c677b8a39711088e
Lee <ab4004@naver.com>
2024-02-02 16:48:35 +0900

    update:0202
    1. 경기&예매 수정
     - 예정으로만 나왔던 버튼 -> 지난 날짜는 종료로 수정
    
    2. 뉴스 추가(미완성)
     - Selenium 이용한 웹 크롤링

--- src/main/java/com/ezpick/lol/EzPickTestApplication.java
+++ src/main/java/com/ezpick/lol/EzPickTestApplication.java
@@ -3,10 +3,15 @@ package com.ezpick.lol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 @SpringBootApplication
 public class EzPickTestApplication {
 
 	public static void main(String[] args) {
+		// WebDriverManager를 사용하여 ChromeDriver 설정
+		WebDriverManager.chromedriver().setup();
+		
 		SpringApplication.run(EzPickTestApplication.class, args);
 	}
 }
